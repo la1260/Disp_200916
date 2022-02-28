1d8bfe94b80b5b89415bc055965377b76a1a2ed8
LHorvath <LHorvath@LHORVATH-NB.pgsm.hu>
2022-02-28 19:07:01 +0100

    Rail és rekordja átnevezve (Rails, Rail); Train és rekordja átnevezve (Trains, Train), utóbbi kimozgatva.

--- src/hu/hl/disp_200916/Main.java
+++ src/hu/hl/disp_200916/Main.java
@@ -11,16 +11,16 @@ import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
 import hu.hl.disp_200916.core.DispCore;
-import hu.hl.disp_200916.core.Rail;
+import hu.hl.disp_200916.core.Rails;
 import hu.hl.disp_200916.core.Route;
 import hu.hl.disp_200916.core.Section;
-import hu.hl.disp_200916.core.Train;
+import hu.hl.disp_200916.core.Trains;
 
 // a kihaladó, utolsó Track-on (Target előtt közvetlenül) ne engedd megfordulni (nem is lenne értelme)
 
 public class Main implements DispCore {
-	private Rail rail= new Rail(this);
-	private Train train= new Train();
+	private Rails rail= new Rails(this);
+	private Trains train= new Trains();
 	private Route route= new Route(rail, train);
 	private Section section= new Section(this, train, route);
 	
@@ -61,38 +61,38 @@ public class Main implements DispCore {
 		frame.getLayeredPane().setOpaque(true);
 		frame.getLayeredPane().setVisible(true);
 		
-		rail.put(0, Rail.Type.J, 0, 6, 8, 9, -1, 72/3.6, 36/3.6, 18, 4, 0, 0);
-		rail.put(1, Rail.Type.J, 0, 7, 11, 10, -1, 72/3.6, 36/3.6, 18, 6, 0, 0);
-		rail.put(2, Rail.Type.J, 0, 8, 12, 10, -1, 72/3.6, 36/3.6, 20, 4, 0, 0);
-		rail.put(3, Rail.Type.J, 0, 11, 13, 9, 14, 72/3.6, 36/3.6, 20, 6, 0, 0);
-		rail.put(4, Rail.Type.J, 0, 15, 16, -1, -1, 72/3.6, -1, 36, 9, 0, 0);
-		rail.put(5, Rail.Type.J, 0, 16, 17, -1, -1, 72/3.6, -1, 36, 11, 0, 0);
-		rail.put(6, Rail.Type.L, 25, 18, 0, -1, -1, -1, -1, 16, 4, 2, 0);
-		rail.put(7, Rail.Type.L, 25, 19, 1, -1, -1, -1, -1, 16, 6, 2, 0);
-		rail.put(8, Rail.Type.L, 25, 0, 2, -1, -1, -1, -1, 18, 4, 2, 0);
-		rail.put(9, Rail.Type.L, 25, 0, 3, 10, -1, -1, -1, 18, 4, 2, 2);
-		rail.put(10, Rail.Type.L, 25, 1, 2, 9, -1, -1, -1, 18, 6, 2, -2);
-		rail.put(11, Rail.Type.L, 25, 1, 3, -1, -1, -1, -1, 18, 6, 2, 0);
-		rail.put(12, Rail.Type.L, 25, 2, 20, -1, -1, -1, -1, 20, 4, 2, 0);
-		rail.put(13, Rail.Type.L, 25, 3, 21, -1, -1, -1, -1, 20, 6, 2, 0);
-		rail.put(14, Rail.Type.L, 25, 3, 24, -1, -1, -1, -1, 20, 6, 2, 2);
-		rail.put(15, Rail.Type.L, 25, 24, 4, -1, -1, -1, -1, 34, 8, 2, 2);
-		rail.put(16, Rail.Type.L, 25, 4, 5, -1, -1, -1, -1, 36, 10, 0, 2);
-		rail.put(17, Rail.Type.L, 25, 5, 25, -1, -1, -1, -1, 34, 14, 2, -2);
-		rail.put(18, Rail.Type.R, 500, 27, 6, -1, -1, 90/3.6, -1, 4, 4, 12, 0);
-		rail.put(19, Rail.Type.R, 500, 28, 7, -1, -1, 90/3.6, -1, 4, 6, 12, 0);
-		rail.put(20, Rail.Type.R, 500, 12, 22, -1, -1, 90/3.6, -1, 22, 4, 12, 0);
-		rail.put(21, Rail.Type.R, 500, 13, 23, -1, -1, 90/3.6, -1, 22, 6, 12, 0);
-		rail.put(22, Rail.Type.R, 500, 20, 29, -1, -1, 90/3.6, -1, 34, 4, 12, 0);
-		rail.put(23, Rail.Type.R, 500, 21, 30, -1, -1, 90/3.6, -1, 34, 6, 12, 0);
-		rail.put(24, Rail.Type.R, 500, 14, 15, -1, -1, 80/3.6, -1, 22, 8, 12, 0);
-		rail.put(25, Rail.Type.R, 500, 17, 26, -1, -1, 90/3.6, -1, 22, 14, 12, 0);
-		rail.put(26, Rail.Type.R, 500, 25, 31, -1, -1, 90/3.6, -1, 10, 14, 12, 0);
-		rail.put(27, Rail.Type.T, 0, 18, -1, -1, -1, -1, -1, 4, 4, 0, 0);
-		rail.put(28, Rail.Type.T, 0, 19, -1, -1, -1, -1, -1, 4, 6, 0, 0);
-		rail.put(29, Rail.Type.T, 0, 22, -1, -1, -1, -1, -1, 46, 4, 0, 0);
-		rail.put(30, Rail.Type.T, 0, 23, -1, -1, -1, -1, -1, 46, 6, 0, 0);
-		rail.put(31, Rail.Type.T, 0, 26, -1, -1, -1, -1, -1, 10, 14, 0, 0);
+		rail.put(0, Rails.Type.J, 0, 6, 8, 9, -1, 72/3.6, 36/3.6, 18, 4, 0, 0);
+		rail.put(1, Rails.Type.J, 0, 7, 11, 10, -1, 72/3.6, 36/3.6, 18, 6, 0, 0);
+		rail.put(2, Rails.Type.J, 0, 8, 12, 10, -1, 72/3.6, 36/3.6, 20, 4, 0, 0);
+		rail.put(3, Rails.Type.J, 0, 11, 13, 9, 14, 72/3.6, 36/3.6, 20, 6, 0, 0);
+		rail.put(4, Rails.Type.J, 0, 15, 16, -1, -1, 72/3.6, -1, 36, 9, 0, 0);
+		rail.put(5, Rails.Type.J, 0, 16, 17, -1, -1, 72/3.6, -1, 36, 11, 0, 0);
+		rail.put(6, Rails.Type.L, 25, 18, 0, -1, -1, -1, -1, 16, 4, 2, 0);
+		rail.put(7, Rails.Type.L, 25, 19, 1, -1, -1, -1, -1, 16, 6, 2, 0);
+		rail.put(8, Rails.Type.L, 25, 0, 2, -1, -1, -1, -1, 18, 4, 2, 0);
+		rail.put(9, Rails.Type.L, 25, 0, 3, 10, -1, -1, -1, 18, 4, 2, 2);
+		rail.put(10, Rails.Type.L, 25, 1, 2, 9, -1, -1, -1, 18, 6, 2, -2);
+		rail.put(11, Rails.Type.L, 25, 1, 3, -1, -1, -1, -1, 18, 6, 2, 0);
+		rail.put(12, Rails.Type.L, 25, 2, 20, -1, -1, -1, -1, 20, 4, 2, 0);
+		rail.put(13, Rails.Type.L, 25, 3, 21, -1, -1, -1, -1, 20, 6, 2, 0);
+		rail.put(14, Rails.Type.L, 25, 3, 24, -1, -1, -1, -1, 20, 6, 2, 2);
+		rail.put(15, Rails.Type.L, 25, 24, 4, -1, -1, -1, -1, 34, 8, 2, 2);
+		rail.put(16, Rails.Type.L, 25, 4, 5, -1, -1, -1, -1, 36, 10, 0, 2);
+		rail.put(17, Rails.Type.L, 25, 5, 25, -1, -1, -1, -1, 34, 14, 2, -2);
+		rail.put(18, Rails.Type.R, 500, 27, 6, -1, -1, 90/3.6, -1, 4, 4, 12, 0);
+		rail.put(19, Rails.Type.R, 500, 28, 7, -1, -1, 90/3.6, -1, 4, 6, 12, 0);
+		rail.put(20, Rails.Type.R, 500, 12, 22, -1, -1, 90/3.6, -1, 22, 4, 12, 0);
+		rail.put(21, Rails.Type.R, 500, 13, 23, -1, -1, 90/3.6, -1, 22, 6, 12, 0);
+		rail.put(22, Rails.Type.R, 500, 20, 29, -1, -1, 90/3.6, -1, 34, 4, 12, 0);
+		rail.put(23, Rails.Type.R, 500, 21, 30, -1, -1, 90/3.6, -1, 34, 6, 12, 0);
+		rail.put(24, Rails.Type.R, 500, 14, 15, -1, -1, 80/3.6, -1, 22, 8, 12, 0);
+		rail.put(25, Rails.Type.R, 500, 17, 26, -1, -1, 90/3.6, -1, 22, 14, 12, 0);
+		rail.put(26, Rails.Type.R, 500, 25, 31, -1, -1, 90/3.6, -1, 10, 14, 12, 0);
+		rail.put(27, Rails.Type.T, 0, 18, -1, -1, -1, -1, -1, 4, 4, 0, 0);
+		rail.put(28, Rails.Type.T, 0, 19, -1, -1, -1, -1, -1, 4, 6, 0, 0);
+		rail.put(29, Rails.Type.T, 0, 22, -1, -1, -1, -1, -1, 46, 4, 0, 0);
+		rail.put(30, Rails.Type.T, 0, 23, -1, -1, -1, -1, -1, 46, 6, 0, 0);
+		rail.put(31, Rails.Type.T, 0, 26, -1, -1, -1, -1, -1, 10, 14, 0, 0);
 		train.put(0, 31, 100, 90/3.6, 2.5, -5);
 		train.put(1, 27, 100, 90/3.6, 2.5, -5);
 		train.put(2, 27, 100, 90/3.6, 2.5, -5);
@@ -215,7 +215,7 @@ public class Main implements DispCore {
 		while (Math.rint(t*10)<4000) {
 			t= Math.rint(section.step(t, 0.1)*10)/10;
 			try {
-				Thread.sleep(10);
+				Thread.sleep(1);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
